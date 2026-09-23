7a3a4e4ae36d5af3db2ac69a1e7b8c4ee9cbf134
veronique-1 <veronikar3107@gmail.com>
2026-09-23 13:13:51 +0300

    laba

--- src/lab01/ex06.py
+++ src/lab01/ex06.py
@@ -1,10 +1,10 @@
-n = int(input())
+n = int(input(f'in_1: '))
 
 ochny = 0   
 zaochny = 0  
 
 for i in range(n):
-    line = input().split()
+    line = input(f'in_{i+2}: ').split()
     if_ochny= line[3] == "True"
 
     if if_ochny:
