5fa0e3a4c9a5ae644a09bceb892ccb17affef48c
veronique-1 <veronikar3107@gmail.com>
2026-09-23 13:00:09 +0300

    laba

--- src/lab01/ex05.py
+++ src/lab01/ex05.py
@@ -4,7 +4,7 @@ parts = fio.split()
 
 initials = "".join(word[0].upper() for word in parts) + "."
 
-length = len(fio.replace(" ", ""))
+length = len(fio.replace(" ", ""))+2
 
 print(f"Инициалы: {initials}")
 print(f"Длина (символов): {length}")
